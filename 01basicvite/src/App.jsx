a76878f506842e37b9c554cd0781bd073cd62b4e
nathanmdj <nathanmdj628@gmail.com>
2024-01-22 10:25:00 +0800

    added projects

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>Vite app</h1>
+  )
+}
+
+export default App
---
+++ 01basicvite/src/youtube.js
@@ -0,0 +1,7 @@
+function youtube(){
+  return (
+    <h1>Youtube</h1>
+  )
+}
+
+export default youtube
